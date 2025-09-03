0975e330e6c62ddaf0d1ceaeda60a2967272241a
Chakri peddi <chakripeddi8@gmail.com>
2025-09-03 19:13:47 +0530

    InfluemcerHomePage locators

---
+++ src/test/java/com/example/pages/InfluencerHomePage.java
@@ -0,0 +1,21 @@
+package com.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class InfluencerHomePage {
+	
+	WebDriver driver;
+	
+	@FindBy(xpath="//h1[text()='Influencer Dashboard']")
+	private WebElement influencerDashboard;
+	
+	
+	
+	
+	public InfluencerHomePage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
